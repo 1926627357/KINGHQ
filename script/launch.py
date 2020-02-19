fb299e3c25a578c190e78fe0c295c8a64a094c18
Haiquan Wang <1926627357@qq.com>
2020-02-19 12:44:54 +0800

    add tcp init_method

--- script/launch.py
+++ script/launch.py
@@ -1,5 +1,21 @@
 import os
 
+pytorch_laucher0 = "/home/v-haiqwa/anaconda3/envs/pytorch/bin/python -m torch.distributed.launch \
+    --nproc_per_node 2 \
+    --nnodes 2 \
+    --node_rank 0 \
+    --master_addr 10.150.144.122 \
+    --master_port 8889 \
+    /home/v-haiqwa/Documents/KINGHQ/test/distributed.py"
 
-out = os.popen('ssh v-haiqwa@10.190.175.223 ls /home/v-haiqwa')
+pytorch_laucher1 = "/home/v-haiqwa/anaconda3/envs/pytorch/bin/python -m torch.distributed.launch \
+    --nproc_per_node 2 \
+    --nnodes 2 \
+    --node_rank 1 \
+    --master_addr 10.150.144.122 \
+    --master_port 8889 \
+    /home/v-haiqwa/Documents/KINGHQ/test/distributed.py"
+
+# out_srgws04 = os.popen('ssh v-haiqwa@10.150.144.122 '+pytorch_laucher0)
+out_srgws10 = os.popen('ssh v-haiqwa@10.190.175.223 '+pytorch_laucher1)
 
--- test/distributed.py
+++ test/distributed.py
@@ -1,5 +1,5 @@
 import torch.distributed as dist
 import torch
 
-dist.init_process_group(backend='gloo')
+dist.init_process_group(backend='gloo', init_method='tcp://10.150.144.122:30000')
 print("I'am the rank:%d"%dist.get_rank())
