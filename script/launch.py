2f9622cf25d2641ea1316285c23ae8be9769a076
Haiquan Wang <1926627357@qq.com>
2020-02-19 12:58:30 +0800

    fix tcp bug

--- script/launch.py
+++ script/launch.py
@@ -16,6 +16,6 @@ pytorch_laucher1 = "/home/v-haiqwa/anaconda3/envs/pytorch/bin/python -m torch.di
     --master_port 8889 \
     /home/v-haiqwa/Documents/KINGHQ/test/distributed.py"
 
-# out_srgws04 = os.popen('ssh v-haiqwa@10.150.144.122 '+pytorch_laucher0)
+out_srgws04 = os.popen('ssh v-haiqwa@10.150.144.122 '+pytorch_laucher0)
 out_srgws10 = os.popen('ssh v-haiqwa@10.190.175.223 '+pytorch_laucher1)
 
--- test/distributed.py
+++ test/distributed.py
@@ -1,5 +1,8 @@
 import torch.distributed as dist
 import torch
+import os
 
-dist.init_process_group(backend='gloo', init_method='tcp://10.150.144.122:30000')
+
+dist.init_process_group(backend='gloo', init_method='tcp://10.150.144.122:30000', 
+                        rank=int(os.environ['RANK']), world_size=int(os.environ['WORLD_SIZE']))
 print("I'am the rank:%d"%dist.get_rank())
