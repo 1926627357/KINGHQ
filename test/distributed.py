7741234697057dc6b5fb6a39530706589615be79
Haiquan Wang <1926627357@qq.com>
2020-02-19 14:09:46 +0800

    mpi4py

--- test/distributed.py
+++ test/distributed.py
@@ -1,8 +1,9 @@
-import torch.distributed as dist
-import torch
-import os
+from mpi4py import MPI
 
 
-dist.init_process_group(backend='gloo', init_method='tcp://10.150.144.122:30000', 
-                        rank=int(os.environ['RANK']), world_size=int(os.environ['WORLD_SIZE']))
-print("I'am the rank:%d"%dist.get_rank())
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+node_name = MPI.Get_processor_name() # get the name of the node
+
+print('Hello world from process %d at %s.'%(rank, node_name))
