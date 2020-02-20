92eb3bec6ac7d5037d9eda85415385dec29051c5
Haiquan Wang <1926627357@qq.com>
2020-02-20 10:58:34 +0800

    update

--- script/update.py
+++ script/update.py
@@ -8,4 +8,4 @@ with open(path,"r") as load_f:
 
 os.popen('cd /home/v-haiqwa/Documents/KINGHQ/ && git add . && git commit -m \"update\" && git push origin master')
 for hostname,ip in slaverlist.items():
-    print("ssh v-haiqwa@"+ip+" cd /home/v-haiqwa/Documents/KINGHQ/ && git pull")
+    os.popen("ssh v-haiqwa@"+ip+" cd /home/v-haiqwa/Documents/KINGHQ/ && git pull")
