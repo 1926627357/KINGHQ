60a42a203c900a8ce597f84cc1fa9e786c651e1a
Haiquan Wang <1926627357@qq.com>
2020-02-20 13:10:31 +0800

    update

--- test/distributed.py
+++ test/distributed.py
@@ -7,4 +7,9 @@ import os
 # os.environ['RANK']='0'
 # os.environ['WORLD_SIZE']='1'
 dist.init_process_group(backend='mpi')
+# import time
+# time.sleep(1)
+t = torch.tensor([float(dist.get_rank())])
+dist.broadcast(t,src=0)
 print("I'am the rank:%d"%dist.get_rank())
+print(t.tolist())
