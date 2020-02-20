9d61df1d3cf7e86c322347409b3bfdfb5ff2ad9f
Haiquan Wang <1926627357@qq.com>
2020-02-20 11:05:13 +0800

    update

--- script/update.py
+++ script/update.py
@@ -1,11 +1,22 @@
 import os
 import json5
-
+###
+#this script is used to update all machines project
 
 path = "/home/v-haiqwa/Documents/KINGHQ/config/host/slaverlist"
 with open(path,"r") as load_f:
     slaverlist=json5.load(load_f)
 
-os.popen('cd /home/v-haiqwa/Documents/KINGHQ/ && git add . && git commit -m \"update\" && git push origin master')
+import subprocess
+
+def excuteCommand(com):
+    ex = subprocess.Popen(com, stdout=subprocess.PIPE, shell=True)
+    out, err  = ex.communicate()
+    status = ex.wait()
+    return out.decode()
+
+print(excuteCommand('cd /home/v-haiqwa/Documents/KINGHQ/ && git add . && git commit -m \"update\" && git push origin master'))
+# os.popen('cd /home/v-haiqwa/Documents/KINGHQ/ && git add . && git commit -m \"update\" && git push origin master')
 for hostname,ip in slaverlist.items():
-    os.popen("ssh v-haiqwa@"+ip+" cd /home/v-haiqwa/Documents/KINGHQ/ && git pull")
+    print("="*30)
+    print(excuteCommand("ssh v-haiqwa@"+ip+" cd /home/v-haiqwa/Documents/KINGHQ/ && git pull"))
