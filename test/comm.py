6009afef893430476b69dc2bf8206f6c78fef610
haiqwa <1926627357@qq.com>
2020-02-27 23:17:08 +0800

    update

--- test/comm.py
+++ test/comm.py
@@ -16,19 +16,20 @@ import time
 t1=time.time()
 for group in optimizer.param_groups:
   for p in group['params']:
-    print("hello!!!%d"%dist.get_rank())
-    if dist.get_rank()==0:
-      p_temp=p.to("cpu")
-      dist.send(tensor=torch.tensor([1.,2.,3.]),dst=1,tag=0)
-      dist.send(tensor=p_temp,dst=1,tag=1)
-      dist.recv(tensor=p_temp,src=1,tag=2)
-      p_temp=p_temp.to(device)
-      p.detach().add_(p_temp)
-    else:
-      p_temp=torch.randn(p.size())
-      dist.recv(tensor=torch.tensor([1.,2.,3.]),src=0,tag=0)
-      dist.recv(tensor=p_temp,src=0,tag=1)
-      dist.send(tensor=p_temp,dst=0,tag=2)
+    #print("hello!!!%d"%dist.get_rank())
+    #if dist.get_rank()==0:
+    #  p_temp=p.to("cpu")
+    #  dist.send(tensor=torch.tensor([1.,2.,3.]),dst=1,tag=0)
+    #  dist.send(tensor=p_temp,dst=1,tag=1)
+    #  dist.recv(tensor=p_temp,src=1,tag=2)
+    #  p_temp=p_temp.to(device)
+    #  p.detach().add_(p_temp)
+    #else:
+    #  p_temp=torch.randn(p.size())
+    #  dist.recv(tensor=torch.tensor([1.,2.,3.]),src=0,tag=0)
+    #  dist.recv(tensor=p_temp,src=0,tag=1)
+    #  dist.send(tensor=p_temp,dst=0,tag=2)
+    print(p)
 t2=time.time()
 if dist.get_rank()==0:
     print(t2-t1)
