fa3b782ff784aeca745a681bcea4592b1cd7c0b7
haiqwa <1926627357@qq.com>
2020-02-28 10:43:02 +0800

    update

--- test/comm.py
+++ test/comm.py
@@ -14,27 +14,31 @@ optimizer=torch.optim.SGD(model.parameters(),lr=0.002)
 import time
 
 t1=time.time()
+n=0
 for group in optimizer.param_groups:
   for p in group['params']:
+    n+=1
     print("hello!!!%d"%dist.get_rank())
     if dist.get_rank()==0:
       p_temp=p.to("cpu")
+      t=torch.tensor([1.,2.,3.])
       print("I am-%d: I try to send [1,2,3]"%dist.get_rank())
-      dist.send(tensor=torch.tensor([1.,2.,3.]),dst=1,tag=0)
+      dist.send(tensor=t,dst=1,tag=1+n*4)
       print("I am-%d: I try to send param"%dist.get_rank())
-      dist.send(tensor=p_temp,dst=1,tag=1)
+      dist.send(tensor=p_temp,dst=1,tag=2+n*4)
       print("I am-%d: I try to recv param"%dist.get_rank())
-      dist.recv(tensor=p_temp,src=1,tag=2)
+      dist.recv(tensor=p_temp,src=1,tag=3+n*4)
       p_temp=p_temp.to(device)
       p.detach().add_(p_temp)
     else:
       p_temp=torch.randn(p.size())
+      t=torch.tensor([1.,2.,3.])
       print("I am-%d: I try to recv [1,2,3]"%dist.get_rank())
-      dist.recv(tensor=torch.tensor([1.,2.,3.]),src=0,tag=0)
+      dist.recv(tensor=t,src=0,tag=1+n*4)
       print("I am-%d: I try to recv param"%dist.get_rank())
-      dist.recv(tensor=p_temp,src=0,tag=1)
+      dist.recv(tensor=p_temp,src=0,tag=2+n*4)
       print("I am-%d: I try to send param"%dist.get_rank())
-      dist.send(tensor=p_temp,dst=0,tag=2)
+      dist.send(tensor=p_temp,dst=0,tag=3+n*4)
     
 t2=time.time()
 if dist.get_rank()==0:
