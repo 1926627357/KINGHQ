d90b44a0edfdc1eb3d5a3e5d47bfbcf088d1b2f8
haiqwa <1926627357@qq.com>
2020-02-29 10:59:06 +0800

    update

--- test/comm.py
+++ test/comm.py
@@ -18,27 +18,28 @@ n=0
 for group in optimizer.param_groups:
   for p in group['params']:
     n+=1
-    #print("hello!!!%d"%dist.get_rank())
-    if dist.get_rank()==0:
-      p_temp=p.to("cpu")
-      t=torch.tensor([1.,2.,3.])
-      #print("I am-%d: I try to send [1,2,3]"%dist.get_rank())
-      dist.send(tensor=t,dst=1,tag=1+n*4)
-      #print("I am-%d: I try to send param"%dist.get_rank())
-      dist.send(tensor=p_temp,dst=1,tag=2+n*4)
-      #print("I am-%d: I try to recv param"%dist.get_rank())
-      dist.recv(tensor=p_temp,src=1,tag=3+n*4)
-      p_temp=p_temp.to(device)
-      p.detach().add_(p_temp)
-    else:
-      p_temp=torch.randn(p.size())
-      t=torch.tensor([1.,2.,3.])
-      #print("I am-%d: I try to recv [1,2,3]"%dist.get_rank())
-      dist.recv(tensor=t,src=0,tag=1+n*4)
-      #print("I am-%d: I try to recv param"%dist.get_rank())
-      dist.recv(tensor=p_temp,src=0,tag=2+n*4)
-      #print("I am-%d: I try to send param"%dist.get_rank())
-      dist.send(tensor=p_temp,dst=0,tag=3+n*4)
+    if p.requires_grad:
+      #print("hello!!!%d"%dist.get_rank())
+      if dist.get_rank()==0:
+        p_temp=p.to("cpu")
+        t=torch.tensor([1.,2.,3.])
+        #print("I am-%d: I try to send [1,2,3]"%dist.get_rank())
+        dist.send(tensor=t,dst=1,tag=1+n*4)
+        #print("I am-%d: I try to send param"%dist.get_rank())
+        dist.send(tensor=p_temp,dst=1,tag=2+n*4)
+        #print("I am-%d: I try to recv param"%dist.get_rank())
+        dist.recv(tensor=p_temp,src=1,tag=3+n*4)
+        p_temp=p_temp.to(device)
+        p.detach().add_(p_temp)
+      else:
+        p_temp=torch.randn(p.size())
+        t=torch.tensor([1.,2.,3.])
+        #print("I am-%d: I try to recv [1,2,3]"%dist.get_rank())
+        dist.recv(tensor=t,src=0,tag=1+n*4)
+        #print("I am-%d: I try to recv param"%dist.get_rank())
+        dist.recv(tensor=p_temp,src=0,tag=2+n*4)
+        #print("I am-%d: I try to send param"%dist.get_rank())
+        dist.send(tensor=p_temp,dst=0,tag=3+n*4)
     
 t2=time.time()
 if dist.get_rank()==0:
--- test/optimization.py
+++ test/optimization.py
@@ -4,19 +4,28 @@ import KINGHQ
 from KINGHQ.models import vgg,lenet,mobilenetv2
 import torch
 
-model=vgg.vgg19()
+model=mobilenetv2.mobilenetv2()
+
 optimizer=torch.optim.SGD(model.parameters(),lr=0.002)
 param_size=0
 
+layer=0
+size_count={}
 for group in optimizer.param_groups:
-    for p in group['params']:
-        size=1
-        for each in p.data.size():
-            size*=each
-            # print(size)
-            param_size+=size
-print(param_size)
-
+  for p in group['params']:
+    layer+=1
+    size=1
+    for each in p.data.size():
+      size*=each
+    if size in size_count:
+      size_count[size]+=1
+    else:
+      size_count[size]=1
+    print("layer NO.{}--{}".format(layer,size))
+    param_size+=size
+print("the whole model size is: ",param_size)
+size_count=sorted(size_count.items(),key = lambda item:item[0])
+print(size_count)
 
 # model=lenet.LeNet5()
 # optimizer=torch.optim.SGD(model.parameters(), lr=0.002)
